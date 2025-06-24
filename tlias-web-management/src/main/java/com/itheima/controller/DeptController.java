5828ef6a1361c32f9213bbd05341bb89d2b79168
WangS <2750241635@qq.com>
2025-06-25 02:47:50 +0800

    工程搭建

---
+++ tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
@@ -0,0 +1,8 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController// 作用等同于@Controller和@ResponseBody的结合
+public class DeptController {
+
+}

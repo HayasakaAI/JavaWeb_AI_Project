4e44c1a9905c2046cc26183a294617d8313f0afe
WangS <2750241635@qq.com>
2025-06-25 04:38:53 +0800

    部门管理-列表查询

--- tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
@@ -1,8 +1,37 @@
 package com.itheima.controller;
 
+import com.itheima.pojo.Dept;
+import com.itheima.pojo.Result;
+import com.itheima.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.zip.CheckedOutputStream;
 
 @RestController// 作用等同于@Controller和@ResponseBody的结合
 public class DeptController {
+
+//    声明一个service接口
+    @Autowired
+    private DeptService deptService;
+
+    @GetMapping("/depts") // 请求方式必须为GET，等同于下面的
+    //@RequestMapping(value = "/depts",method = RequestMethod.GET) // method属性指定请求方式
+    public Result list() {
+
+        System.out.println("查询所有部门数据");
+        List<Dept> deptList =  deptService.findAll();
+
+       // 把deptList封装到Result对象中
+        Result result = Result.success(deptList);
+
+        // 返回Result对象
+        return result;
+    }
+
 
 }
--- tlias-web-management/src/main/java/com/itheima/service/DeptService.java
+++ tlias-web-management/src/main/java/com/itheima/service/DeptService.java
@@ -1,4 +1,11 @@
 package com.itheima.service;
 
+import com.itheima.pojo.Dept;
+
+import java.util.List;
+
 public interface DeptService {
+
+    /*查询所有部门*/
+    List<Dept> findAll();
 }
